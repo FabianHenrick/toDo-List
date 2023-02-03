9c1d60650638e547f4cf234e391451b880440b87
FabianHenrick <fabianhenrick@gmail.com>
2023-02-03 00:12:09 -0300

    Continuar Depois

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,39 +1,34 @@
-import { Trash } from "phosphor-react"
-import style from './Task.module.css'
+import { Trash } from "phosphor-react";
+import { HTMLProps, useState } from "react";
+import { ChangeEvent, InputHTMLAttributes } from "react";
+import style from "./Task.module.css";
 
-
-
-interface taskProps{
-    
-    content:string;
-    onDeleteTask:(tasks:string) => void;
- 
+interface taskProps {
+  content: string;
+  onDeleteTask: (tasks: string) => void;
 }
 
+export function Task(props: taskProps) {
+  function handleDeleteTask() {
+    props.onDeleteTask(props.content);
+  }
 
-export function Task(props:taskProps){
-   
-   function handleDeleteTask(){
-    props.onDeleteTask(props.content)
-   }
+  const [checkedState, setCheckedState] = useState<boolean>(false);
 
-   return(
+  return (
+    <div className={style.task}>
+      <label className={style.container}>
+        <input
+          className={style.checkbox}
+          type="checkbox"
+          checked={checkedState}
+          onChange={(event) => setCheckedState(event?.target.checked)}
+        ></input>
+        <span className={style.checkmark}></span>
+      </label>
+      <p className={style.taskLabel}>{props.content}</p>
 
-    
-            <div className={style.task}>
-                <label className={style.container}>
-                    <input className={style.checkbox} type='checkbox'></input>
-                    <span className={style.checkmark}></span>
-                </label> 
-                    <p className={style.taskLabel}>{props.content}</p>
-                   
-                <Trash
-                    className={style.trashIcon}
-                    size='24'
-                    onClick={handleDeleteTask}
-                />
-                    
-            </div>
-    
-    )
+      <Trash className={style.trashIcon} size="24" onClick={handleDeleteTask} />
+    </div>
+  );
 }
--- src/components/Tasker.tsx
+++ src/components/Tasker.tsx
@@ -1,86 +1,76 @@
-import style from './Tasker.module.css'
-import {FormEvent ,useState, ChangeEvent} from 'react'
-import { Task } from './Task';
-
-
+import style from "./Tasker.module.css";
+import { FormEvent, useState, ChangeEvent } from "react";
+import { Task } from "./Task";
 
 export function Tasker() {
+  const [tasks, setTasks] = useState<string[]>([]);
 
-    let [tasks, setTasks] =useState<string[]>([])
+  const [newTaskText, setNewTaskText] = useState("");
 
-    let [newTaskText, setNewTaskText] = useState('')
+  const [taskCount, setTaskCont] = useState<number>(0);
 
-    let [taskCount, setTaskCont] = useState<number>(0)
+  const [taskCompleted, setTaskeCompleted] = useState<number>(0);
 
-    let [taskCompleted, setTaskeCompleted] = useState<number>(taskCount)
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+    setTasks([...tasks, newTaskText]);
 
+    setNewTaskText("");
+    setTaskCont(taskCount + 1);
+  }
 
-    function handleCreateNewTask(event:FormEvent){
-        event.preventDefault()
-        setTasks([...tasks, newTaskText])
-        
-        setNewTaskText('')
-        setTaskCont(taskCount +1)
-    }
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event?.target.value);
+  }
 
-    function handleNewTaskChange(event:ChangeEvent<HTMLInputElement>){
-        setNewTaskText(event?.target.value)
-    }
+  function deleteTask(taskToDelete: string) {
+    const tasksWithoutDeletedOne = tasks.filter((tasks) => {
+      return tasks !== taskToDelete;
+    });
+    setTaskCont(taskCount - 1);
+    setTasks(tasksWithoutDeletedOne);
+  }
 
-    function deleteTask(taskToDelete:string){
-
-        const tasksWithoutDeletedOne = tasks.filter(tasks =>{
-            return tasks !==  taskToDelete
-        })
-        setTaskCont(taskCount-1)
-        setTasks(tasksWithoutDeletedOne)
-    }
-
-    return(
-        <div className={style.tasker}>
-            <div className={style.taskDiv}>
-                <input 
-                    type='textarea' 
-                    className={style.input} 
-                    placeholder='Adicione suas tarefas aqui' 
-                    onChange={handleNewTaskChange}
-                    value={newTaskText}
-                    
-                    ></input>
-                <button 
-                    className={style.button} 
-                    type='button'
-                    onClick={handleCreateNewTask}
-                    
-                >Criar <img src="./src/assets/plus.svg" className={style.plus} alt="plus" /></button>
-            </div>
-            <div className={style.tasker}>
-            <div className={style.progress}>
-              <div className={style.taskCounters}>
-                <p className={style.taskNumber}>Tarefas criadas</p>
-                <p className={style.taskCounter}>{taskCount}</p>
-              </div>
-              <div className={style.taskCounters}>
-                <p className={style.taskFinished}>Concluídas</p>
-                <p className={style.taskCounter}>{`${taskCount} de ${taskCompleted}`}</p>
-              </div>
-            </div >
-              <div className={style.taskList}>
-              {tasks.map(tasks => {
-                return(
-                    <Task
-                        key={tasks}
-                        content={tasks}
-                        onDeleteTask={deleteTask}
-                        
-                    />
-                )})}
-              
-                
-            </div>
+  return (
+    <div className={style.tasker}>
+      <div className={style.taskDiv}>
+        <input
+          type="textarea"
+          className={style.input}
+          placeholder="Adicione suas tarefas aqui"
+          onChange={handleNewTaskChange}
+          value={newTaskText}
+        ></input>
+        <button
+          className={style.button}
+          type="button"
+          onClick={handleCreateNewTask}
+        >
+          Criar{" "}
+          <img src="./src/assets/plus.svg" className={style.plus} alt="plus" />
+        </button>
+      </div>
+      <div className={style.tasker}>
+        <div className={style.progress}>
+          <div className={style.taskCounters}>
+            <p className={style.taskNumber}>Tarefas criadas</p>
+            <p className={style.taskCounter}>{taskCount}</p>
+          </div>
+          <div className={style.taskCounters}>
+            <p className={style.taskFinished}>Concluídas</p>
+            <p
+              className={style.taskCounter}
+            >{`${taskCompleted} de ${taskCount}`}</p>
           </div>
         </div>
-
-    )
-
+        <div className={style.taskList}>
+          {tasks.map((tasks) => {
+            return (
+              <Task key={tasks} content={tasks} onDeleteTask={deleteTask} />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
